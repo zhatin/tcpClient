89a8876d66f6f550629edff8f4fd1af1067ae47d
zhangting3 <zhatin@163.com>
2017-06-19 10:10:55 +0800

    Init.

---
+++ src/main/java/com/bdreport/test/client/TestClient.java
@@ -0,0 +1,46 @@
+package com.bdreport.test.client;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+public final class TestClient {
+
+    static final String HOST = System.getProperty("host", "192.168.0.100");
+    static final int PORT = Integer.parseInt(System.getProperty("port", "8827"));
+    static final int SIZE = Integer.parseInt(System.getProperty("size", "256"));
+
+    public static void main(String[] args) throws Exception {
+        // Configure SSL.git
+        // Configure the client.
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+             .channel(NioSocketChannel.class)
+             .option(ChannelOption.TCP_NODELAY, true)
+             .handler(new ChannelInitializer<SocketChannel>() {
+                 @Override
+                 public void initChannel(SocketChannel ch) throws Exception {
+                     ChannelPipeline p = ch.pipeline();
+                     p.addLast(new TestClientHandler());
+                 }
+             });
+
+            // Start the client.
+            ChannelFuture f = b.connect(HOST, PORT).sync();
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } finally {
+            // Shut down the event loop to terminate all threads.
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ src/main/java/com/bdreport/test/client/TestClientHandler.java
@@ -0,0 +1,57 @@
+package com.bdreport.test.client;
+
+import org.apache.commons.codec.binary.Hex;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+
+public class TestClientHandler extends ChannelInboundHandlerAdapter {
+
+	private final ByteBuf firstMessage;
+	private byte[] testMsg = { (byte) 0xEE, (byte) 0xCA, (byte) 0x0C, (byte) 0xA1, (byte) 0x00, (byte) 0x5A,
+			(byte) 0xA2, (byte) 0x00, (byte) 0x5A, (byte) 0xA3, (byte) 0x00, (byte) 0x5A, (byte) 0xA4, (byte) 0x00,
+			(byte) 0x5A, (byte) 0xF2, (byte) 0xFF, (byte) 0xFC, (byte) 0xFF, (byte) 0xFF, (byte) 0xCA, (byte) 0x0C,
+			(byte) 0xA1, (byte) 0x00, (byte) 0x5A, (byte) 0xA2, (byte) 0x00, (byte) 0x5A, (byte) 0xA3, (byte) 0xEE,
+			(byte) 0xCA, (byte) 0x0C, (byte) 0xA1, (byte) 0x00, (byte) 0x5A, (byte) 0xA2, (byte) 0x00, (byte) 0x5A,
+			(byte) 0xA3, (byte) 0x00, (byte) 0x5A, (byte) 0xA4, (byte) 0x00, (byte) 0x5A, (byte) 0xF2, (byte) 0xFF,
+			(byte) 0xFC, (byte) 0xFF, (byte) 0xFF, (byte) 0xEE, (byte) 0xCA, (byte) 0x0C, (byte) 0xA1, (byte) 0x00,
+			(byte) 0x5A, (byte) 0xA2, (byte) 0x00, (byte) 0x5A, (byte) 0xA3 };
+	private byte[] testPackageB1 = { (byte) 0xEE, (byte) 0xB1, (byte) 0x00, (byte) 0x01, (byte) 0x07, (byte) 0xE1,
+			(byte) 0x06, (byte) 0x0D, (byte) 0x09, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x04, (byte) 0x4E,
+			(byte) 0x6C, (byte) 0x4E, (byte) 0x73, (byte) 0x7B, (byte) 0xFF, (byte) 0xFC, (byte) 0xFF, (byte) 0xFF };
+	private String testStr = "test message.\n";
+
+	public TestClientHandler() {
+		firstMessage = Unpooled.wrappedBuffer(testPackageB1);
+	}
+
+	@Override
+	public void channelActive(ChannelHandlerContext ctx) {
+		ctx.writeAndFlush(firstMessage);
+		String hexStr = Hex.encodeHexString(testPackageB1).toUpperCase();
+		System.out.println("Sent Message: " + hexStr);
+	}
+
+	@Override
+	public void channelRead(ChannelHandlerContext ctx, Object msg) {
+		ByteBuf byteBuf = (ByteBuf) msg;
+		byte[] hexByte = new byte[byteBuf.readableBytes()];
+		byteBuf.readBytes(hexByte);
+		String hexStr = Hex.encodeHexString(hexByte).toUpperCase();
+		System.out.println("Received Response: " + hexStr);
+		byteBuf.release();
+	}
+
+	@Override
+	public void channelReadComplete(ChannelHandlerContext ctx) {
+		ctx.close();
+	}
+
+	@Override
+	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+		cause.printStackTrace();
+		ctx.close();
+	}
+}
