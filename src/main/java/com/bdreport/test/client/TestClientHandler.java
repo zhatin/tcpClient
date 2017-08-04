735c1b2725dabf36519704b906d656e13cca0383
ZHATIN <ZHATIN@zhatin-vaio>
2017-08-04 11:33:26 +0800

    add custom 0.0625 format float support.

--- src/main/java/com/bdreport/test/client/TestClientHandler.java
+++ src/main/java/com/bdreport/test/client/TestClientHandler.java
@@ -18,20 +18,26 @@ public class TestClientHandler extends ChannelInboundHandlerAdapter {
 			(byte) 0xA3, (byte) 0x00, (byte) 0x5A, (byte) 0xA4, (byte) 0x00, (byte) 0x5A, (byte) 0xF2, (byte) 0xFF,
 			(byte) 0xFC, (byte) 0xFF, (byte) 0xFF, (byte) 0xEE, (byte) 0xCA, (byte) 0x0C, (byte) 0xA1, (byte) 0x00,
 			(byte) 0x5A, (byte) 0xA2, (byte) 0x00, (byte) 0x5A, (byte) 0xA3 };
-	private byte[] testPackageB1 = { (byte) 0xEE, (byte) 0xB1, (byte) 0x00, (byte) 0x01, (byte) 0x07, (byte) 0xE1,
-			(byte) 0x06, (byte) 0x0D, (byte) 0x09, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x08, (byte) 0x00,
-			(byte) 0x01, (byte) 0x00, (byte) 0x02, (byte) 0x4E, (byte) 0x6C, (byte) 0x4E, (byte) 0x73, (byte) 0x7E,
+	private byte[] testPackageB1half = { (byte) 0xEE, (byte) 0xB1, (byte) 0x00, (byte) 0x01, (byte) 0x07, (byte) 0xE1,
+			(byte) 0x06, (byte) 0x0D, (byte) 0x09, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x10, (byte) 0x00,
+			(byte) 0x01, (byte) 0x00, (byte) 0x02, (byte) 0x4E, (byte) 0x6C, (byte) 0x4E, (byte) 0x73, (byte) 0x00,
+			(byte) 0x02, (byte) 0x00, (byte) 0x02, (byte) 0x4E, (byte) 0x6C, (byte) 0x4E, (byte) 0x73, (byte) 0xFD,
 			(byte) 0xFF, (byte) 0xFC, (byte) 0xFF, (byte) 0xFF };
+	private byte[] testPackageB1custom0625 = { (byte) 0xEE, (byte) 0xB1, (byte) 0x00, (byte) 0x01, (byte) 0x07,
+			(byte) 0xE1, (byte) 0x06, (byte) 0x0D, (byte) 0x09, (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x10,
+			(byte) 0x00, (byte) 0x01, (byte) 0x00, (byte) 0x02, (byte) 0x07, (byte) 0xD0, (byte) 0x01, (byte) 0x91,
+			(byte) 0x00, (byte) 0x02, (byte) 0x00, (byte) 0x02, (byte) 0xFF, (byte) 0x6F, (byte) 0xFC, (byte) 0x90,
+			(byte) 0x6A, (byte) 0xFF, (byte) 0xFC, (byte) 0xFF, (byte) 0xFF };
 	private String testStr = "test message.\n";
 
 	public TestClientHandler() {
-		firstMessage = Unpooled.wrappedBuffer(testPackageB1);
+		firstMessage = Unpooled.wrappedBuffer(testPackageB1custom0625);
 	}
 
 	@Override
 	public void channelActive(ChannelHandlerContext ctx) {
 		ctx.writeAndFlush(firstMessage);
-		String hexStr = Hex.encodeHexString(testPackageB1).toUpperCase();
+		String hexStr = Hex.encodeHexString(testPackageB1custom0625).toUpperCase();
 		System.out.println("Sent Message: " + hexStr);
 	}
 
